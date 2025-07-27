115eb986cbf8ac49e2807ea261f226a6edec187e
elghandori1 <mohamedelghandori4@gmail.com>
2025-07-27 17:54:20 +0100

    app.liste

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ app.use('/match', matchRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
